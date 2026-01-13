03a74e983dcfd7a49b7ed0ecc496cb69a5179a92
Leonardo <lbeduschi.i@gmail.com>
2026-01-13 15:05:09 -0300

    config.js adicionado

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const SUPABASE_CONFIG = {
+    url: 'https://gumuybczgsmfvqrsgslr.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1bXV5YmN6Z3NtZnZxcnNnc2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMTE3MTYsImV4cCI6MjA4Mzg4NzcxNn0.ywZ6gcFAqBtkkV5xSOhuu-SAKDziGEpWRezGr3vM99c'
+};
